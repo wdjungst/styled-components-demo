3860abcb72076fb5d7e741feb00e787c0c3eb38e
David Jungst <djungst@gmail.com>
2017-07-13 10:21:27 -0600

    add some styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { 
+  Header,
+  Button,
+  Segment,
+  Card, 
+  Icon,
+} from 'semantic-ui-react';
+import styled from 'styled-components';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+const AppContainer = styled.div`
+  background: linear-gradient(to bottom, aliceblue, black);
+`
+
+const Transparent = styled.div`
+  background: transparent !important;
+`
+
+const fontSize = (size) => {
+  switch(size) {
+    case 'large':
+      return '4rem';
+    case 'small':
+      return '1rem';
+    default:
+      return '2rem';
   }
 }
+
+const HeaderText = styled.h1`
+  color: white !important;
+  text-align: center;
+  font-size: ${ props =>fontSize(props.fSize)} !important;
+`
+
+
+const App = () => (
+  <AppContainer>
+    <Header 
+      fSize="large"
+      as={ (props) => <HeaderText {...props} />} 
+    >
+      My Portfolio
+    </Header>
+
+    <Segment as={Transparent}>
+      <Header as={HeaderText} >My Projects</Header>
+    </Segment>
+
+    <Segment as={Transparent}>
+      <Header 
+        fSize="small" 
+        as={(props) => <HeaderText {...props} />} 
+      >
+        Contact
+      </Header>
+    </Segment>
+
+  </AppContainer>
+)
 
 export default App;
